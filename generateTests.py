6e75adbb2fe71a5ed0015a2abc584727e7906703
varshit97 <varshit97@gmail.com>
2016-09-24 00:16:30 +0530

    Added script to generate test data randomly from dataset

---
+++ generateTests.py
@@ -0,0 +1,40 @@
+import os
+import random
+import commands
+
+files = []
+classes = {
+        'sport': 511,
+        'tech': 401,
+        'politics': 417,
+        'entertainment': 386,
+        'business': 510
+        }
+
+def getInput():
+    isEmpty = commands.getoutput("ls ./Input/sport")
+    if isEmpty != '':
+        for types in classes.keys():
+            filePath = './Input/' + types
+            os.system("mv %s/* ./datasets/%s/" %(filePath, types))
+        for types in classes.keys():
+            while len(files) <= 50:
+                fileNumber = random.randint(1, classes[types])
+                fileName = "{0:03}".format(fileNumber) + '.txt'
+                if fileName not in files:
+                    files.append(fileName)
+                    filePath = './datasets/' + types + '/' + fileName
+                    os.system("mv %s ./Input/%s" %(filePath, types))
+            del files[:]
+    else:
+        for types in classes.keys():
+            while len(files) <= 50:
+                fileNumber = random.randint(1, classes[types])
+                fileName = "{0:03}".format(fileNumber) + '.txt'
+                if fileName not in files:
+                    files.append(fileName)
+                    filePath = './datasets/' + types + '/' + fileName
+                    os.system("mv %s ./Input/%s" %(filePath, types))
+            del files[:]
+
+getInput()
